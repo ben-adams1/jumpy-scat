67ac7612189ef1f824092d94e44fb16b4a67966f
Ben Adams <67019970+ben-adams1@users.noreply.github.com>
2025-05-14 20:15:51 -0500

    Cleared flags before probe evaluation

--- probe.ts
+++ probe.ts
@@ -1,6 +1,6 @@
 when I receive (ghostProbe)
   {
-    set ghost effect to (100)                                       // Hide the probe
+    set (ghost) effect to (100)                                       // Hide the probe
   }
 
 when I receive (moveProbe)
@@ -9,37 +9,43 @@ when I receive (moveProbe)
 }
 
 when I receive (probeUp)
+set (hasTouchedCeiling) to false
+set (hasTouchedDanger) to false
 {
-  if (touching [level1Ceiling] == true)
+  if (touching (level1Ceiling))
   {
     set (hasTouchedCeiling) to true
   }
-   if (touching [danger] == true)
+   if (touching (danger))
   {
     set (hasTouchedDanger) to true
   }
 }
 
 when I receive (probeDown)
+set (hasTouchedGround) to false
+set (hasTouchedDanger) to false
 {
-  if (touching [level1ground] == true) 
+  if (touching (level1ground)) 
   {
     set (hasTouchedGround) to true
   }
-   if (touching [danger] == true)
+   if (touching (danger))
   {
     set (hasTouchedDanger) to true
   }
 }  
 
 when I receive (probeSideways)
+set (hasTouchedWall) to false
+set (hasTouchedDanger) to false
 {
-  if (touching [level1wall] ==  true) 
+  if (touching (level1wall)) 
   {
     set (hasTouchedWall) to true
   }
   
-  if (touching [danger] == true)
+  if (touching (danger))
   {
     set (hasTouchedDanger) to true
   }
